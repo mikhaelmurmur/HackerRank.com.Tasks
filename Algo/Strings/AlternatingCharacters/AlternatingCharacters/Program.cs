48ec23eb83bf056815db9f4fea3f6277666cc68d
Mikhaelmurmur <stolovich@yahoo.com>
2015-10-24 14:18:25 +0300

    Alternating Characters is done.

---
+++ Algo/Strings/AlternatingCharacters/AlternatingCharacters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int testNumber = int.Parse(Console.ReadLine());
+        while (testNumber-- > 0)
+        {
+            string checkLine = Console.ReadLine();
+            int deleteNumber = 0;
+            for (int letterIndex = 1; letterIndex < checkLine.Length; letterIndex++)
+            {
+                if (checkLine[letterIndex] == checkLine[letterIndex - 1])
+                {
+                    deleteNumber++;
+                }
+            }
+            Console.WriteLine(deleteNumber);
+        }
+    }
+}
