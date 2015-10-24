3028ba4296c3664be95daa031a8a2d98ce424237
Mikhaelmurmur <stolovich@yahoo.com>
2015-10-24 21:01:07 +0300

    Two strings is done

---
+++ Algo/Strings/TwoStrings/TwoStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    static HashSet<char> alphabet = new HashSet<char>() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+        'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w','x','y','z' };
+    static void Main(string[] args)
+    {
+        int testNumber = int.Parse(Console.ReadLine());
+        while (testNumber-- > 0)
+        {
+            string firstString = Console.ReadLine();
+            string secondString = Console.ReadLine();
+            bool isSubstringAvailable = alphabet.Any(letter => firstString.Contains(letter.ToString()) && secondString.Contains(letter.ToString()));
+            Console.WriteLine(isSubstringAvailable ? "YES" : "NO");
+        }
+    }
+}
