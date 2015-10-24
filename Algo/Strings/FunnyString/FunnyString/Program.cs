a7fc38b1d0e07f32eac7aec69ccf507bdac92614
Mikhaelmurmur <stolovich@yahoo.com>
2015-10-24 14:03:38 +0300

    Strings/FunnyString is done

---
+++ Algo/Strings/FunnyString/FunnyString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Solution
+{
+    private static int GetDifference(char first, char second)
+    {
+        if (first >= second)
+        {
+            return (int)first - second;
+        }
+        else
+        {
+            return (int)second - first;
+        }
+    }
+    static void Main(string[] args)
+    {
+        var testNumber = int.Parse(Console.ReadLine());
+        while (testNumber-- > 0)
+        {
+            string funnyString = Console.ReadLine();
+            bool isFunny = true;
+            for (var charIndex = 0; charIndex < funnyString.Length / 2 && isFunny; charIndex++)
+            {
+                isFunny = GetDifference(funnyString[charIndex], funnyString[charIndex + 1]) ==
+                          GetDifference(funnyString[funnyString.Length - 1 - charIndex],
+                              funnyString[funnyString.Length - 2 - charIndex]);
+            }
+            Console.WriteLine(isFunny ? "Funny" : "Not Funny");
+        }
+    }
+}
