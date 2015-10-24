16d4e80db9e29c8cc5382b85ee7438e0dc981bf9
Mikhaelmurmur <stolovich@yahoo.com>
2015-10-24 15:57:01 +0300

    Game of Thrones I is done

---
+++ Algo/Strings/GameofThrones-I/GameofThrones-I/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Dictionary<char, int> alphabet = new Dictionary<char, int>();
+        string key = Console.ReadLine();
+        foreach (char letter in key)
+        {
+            if (!alphabet.ContainsKey(letter))
+            {
+                alphabet.Add(letter, 0);
+            }
+            alphabet[letter]++;
+        }
+        int oddNumber = alphabet.Count(pair => pair.Value % 2 == 1);
+        Console.WriteLine(oddNumber > 1 ? "NO" : "YES");
+    }
+}
