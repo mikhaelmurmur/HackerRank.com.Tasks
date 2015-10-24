6fe7f4a736cb4c1469033d3de046e190c228d9c8
Mikhaelmurmur <stolovich@yahoo.com>
2015-10-24 16:17:42 +0300

    Gemstones is done

---
+++ Algo/Strings/Gemstones/Gemstones/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    static HashSet<char> alphabet = new HashSet<char>() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+        'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w','x','y','z' };
+    static void Main(string[] args)
+    {
+        int rockNumber = int.Parse(Console.ReadLine());
+        List<string> rocks = new List<string>();
+        
+        while (rockNumber-- > 0)
+        {
+            rocks.Add(Console.ReadLine());
+        }
+        int gemNumber = alphabet.Select(letter => rocks.All(rock => rock.Contains(letter))).Count(isGem => isGem);
+        Console.WriteLine(gemNumber);
+    }
+}
