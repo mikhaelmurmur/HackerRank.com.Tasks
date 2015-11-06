13e74f916495a7ba75da9f57bea91732cd898ad1
Mikhaelmurmur <stolovich@yahoo.com>
2015-11-06 22:17:47 +0200

    The love-letter nystery is done!

---
+++ Algo/Strings/TheLoveLetterMystery/TheLoveLetterMystery/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int testNumber = int.Parse(Console.ReadLine());
+        while (testNumber-- > 0)
+        {
+            string plainLetter = Console.ReadLine();
+            int difference = 0;
+            for (int letterIndex = 0; letterIndex < plainLetter.Length / 2; letterIndex++)
+            {
+                difference +=
+                    Math.Abs((int)plainLetter[letterIndex] - (int)plainLetter[plainLetter.Length - 1 - letterIndex]);
+            }
+            Console.WriteLine(difference);
+        }
+    }
+}
